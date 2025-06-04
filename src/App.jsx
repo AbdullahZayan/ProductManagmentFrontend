53de7f129e0ef872b2b7d960163806ee981ff603
Abdullah <abdullahzayan.my@gmail.com>
2025-06-05 02:24:04 +0800

    Added Something

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import ProductPage from "./pages/ProductPage"; 
+import { useEffect, useState } from "react";
 
 function App() {
-  const token = localStorage.getItem("token");
+  // const token = localStorage.getItem("token");
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+   useEffect(() => {
+    const handleStorage = () => {
+      setToken(localStorage.getItem("token"));
+    };
+    window.addEventListener("storage", handleStorage);
+    return () => window.removeEventListener("storage", handleStorage);
+  }, []);
 
  const api = import.meta.env.VITE_APP_API_KEY;
 
